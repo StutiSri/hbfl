ab0c2de33985aa9556786ec4d2aa6c6313e10d8a
StutiSri <stutisrivastv@gmail.com>
2020-05-02 23:02:44 +0530

    Create ec2 from ami from marketplace

--- scripts/module_02/marketplace-ec2-startup.sh
+++ scripts/module_02/marketplace-ec2-startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install git
-git clone https://github.com/ryanmurakami/hbfl.git /home/bitnami/hbfl
+git clone https://github.com/StutiSri/hbfl.git
+git checkout aws_test
 chown -R bitnami: /home/bitnami/hbfl
 cd /home/bitnami/hbfl
 sudo npm i
 sudo npm run start
 
 # The above commands base64 encoded for entering into UserData
-# IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQgL2hvbWUvYml0bmFtaS9oYmZsDQpjaG93biAtUiBiaXRuYW1pOiAvaG9tZS9iaXRuYW1pL2hiZmwNCmNkIC9ob21lL2JpdG5hbWkvaGJmbA0Kc3VkbyBucG0gaQ0Kc3VkbyBucG0gcnVuIHN0YXJ0
+# IyEvYmluL2Jhc2gKc3VkbyBhcHQtZ2V0IHVwZGF0ZQpzdWRvIGFwdC1nZXQgLXkgaW5zdGFsbCBnaXQKZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9TdHV0aVNyaS9oYmZsLmdpdApnaXQgY2hlY2tvdXQgYXdzX3Rlc3QKY2hvd24gLVIgYml0bmFtaTogL2hvbWUvYml0bmFtaS9oYmZsCmNkIC9ob21lL2JpdG5hbWkvaGJmbApzdWRvIG5wbSBpCnN1ZG8gbnBtIHJ1biBzdGFydA==
