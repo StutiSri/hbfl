8dc1672e10af67efdc9b6353e419a89e990fe045
StutiSri <stutisrivastv@gmail.com>
2020-05-03 01:20:51 +0530

    Reuse ebs

--- scripts/module_04/create-ec2-instance.js
+++ scripts/module_04/create-ec2-instance.js
@@ -1,27 +1,29 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your regions */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const ec2 = new AWS.EC2()
-const sgName = 'hamster_sg'
-const keyName = 'hamster_key'
-const instanceId = '/* TODO: Add the instance Id to stop */'
+const sgName = 'hamster_ss_sg_3'
+const keyName = 'hamster_ss_key_3'
+const instanceId = 'i-0afd6c105d08c753e' //deleted the ec2 created from bitnami ami, so terminating the ec2
+//created by the autoscaling group
 
 stopInstance(instanceId)
 .then(() => createInstance(sgName, keyName))
 .then((data) => console.log('Created instance with:', data))
 
 function createInstance (sgName, keyName) {
+console.log("createInstance")
   const params = {
-    ImageId: '/* TODO: Add ami id for aws linux */',
+    ImageId: 'ami-0323c3dd2da7fb37d',
     InstanceType: 't2.micro',
     KeyName: keyName,
     MaxCount: 1,
     MinCount: 1,
     Placement: {
-      AvailabilityZone: '/* TODO: Add the az from the instance that is stopping */'
+      AvailabilityZone: 'us-east-1a'
     },
     SecurityGroups: [
       sgName
@@ -42,7 +44,8 @@ function stopInstance (instanceId) {
   }
 
   return new Promise((resolve, reject) => {
-    ec2.stopInstances(params, (err) => {
+  console.log("stop")
+  ec2.stopInstances(params, (err) => {
       if (err) reject(err)
       else resolve()
     })
--- scripts/module_04/reuse-ebs-volume.js
+++ scripts/module_04/reuse-ebs-volume.js
@@ -1,28 +1,42 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const ec2 = new AWS.EC2()
-const volumeId = '/* TODO: Add the volume to detach/attach */'
-const instanceId = '/* TODO: Add the instance to attach to */'
+const volumeId = 'vol-06b6716e9ba865cf7' //vol id of the stopped instance
+const instanceId = 'i-0b6997de4b15c36a8' //instance created by module_04/create-ec2-instance
 
-detachVolume(volumeId)
-.then(() => attachVolume(instanceId, volumeId))
+//detachVolume(volumeId)
+//.then(() => attachVolume(instanceId, volumeId))
+
+attachVolume(instanceId, volumeId)
 
 function detachVolume (volumeId) {
-  // TODO: Configure detachVolume params
+  const params = {
+    VolumeId: volumeId
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Detach the volume
+    ec2.detachVolume(params, (err, data) => {
+        if(err) reject(err)
+        else resolve(data)
+    })
   })
 }
 
 function attachVolume (instanceId, volumeId) {
-  // TODO: Configure attachVolume params
+  const params = {
+    InstanceId: instanceId,
+    VolumeId: volumeId,
+    Device: '/dev/sdf'
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Attach the volume
+    ec2.attachVolume(params, (err, data) => {
+        if(err) reject(err)
+        else resolve(data)
+    })
   })
 }
