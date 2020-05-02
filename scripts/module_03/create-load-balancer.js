72cda658fe231b35b74f3028419fe944bc2e7e21
StutiSri <stutisrivastv@gmail.com>
2020-05-02 23:26:29 +0530

    Create load balancer

--- scripts/module_03/create-load-balancer.js
+++ scripts/module_03/create-load-balancer.js
@@ -2,16 +2,18 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-// TODO: Create a new ELBv2 object
-const sgName = 'hamsterELBSG'
-const tgName = 'hamsterTG'
-const elbName = 'hamsterELB'
-const vpcId = '/* TODO: Add your VPC Id */'
+const elbv2 = new AWS.ELBv2()
+
+const sgName = 'hamsterELBSG_ss'
+const tgName = 'hamsterTG-ss'
+const elbName = 'hamsterELB-ss'
+const vpcId = 'vpc-78faf702'
 const subnets = [
-  /* TODO: Add two subnets */
+  'subnet-b2d72893',
+  'subnet-d5f87898'
 ]
 
 helpers.createSecurityGroup(sgName, 80)
@@ -30,7 +32,21 @@ helpers.createSecurityGroup(sgName, 80)
 .then((data) => console.log(data))
 
 function createLoadBalancer (lbName, sgId) {
-  // TODO: Create a load balancer
+  const params = {
+    Name: lbName,
+    Subnets: subnets,
+    SecurityGroups: [
+        sgId
+    ]
+  }
+
+  return new Promise((resolve, reject) => {
+    elbv2.createLoadBalancer(params, (err, data) => {
+        if(err) reject(err)
+        else resolve(data)
+    })
+  })
+
 }
 
 function createTargetGroup (tgName) {
