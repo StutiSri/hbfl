74b51d00fad327a89f134f4f1e7eed9adb5ec988
StutiSri <stutisrivastv@gmail.com>
2020-05-02 23:01:54 +0530

    Create ec2 instance via SDK

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -1,13 +1,15 @@
 // Imports
-// TODO: Import the aws-sdk
+const AWS = require ('aws-sdk')
 const helpers = require('./helpers')
 
-// TODO: Configure region
+AWS.config.update({region: "us-east-1"})
 
 // Declare local variables
-// TODO: Create an ec2 object
-const sgName = 'hamster_sg'
-const keyName = 'hamster_key'
+
+const ec2 = new AWS.EC2()
+
+const sgName = 'hamster_ss_sg_3'
+const keyName = 'hamster_ss_key_3'
 
 // Do all the things together
 createSecurityGroup(sgName)
@@ -28,13 +30,92 @@ createSecurityGroup(sgName)
 // Create functions
 
 function createSecurityGroup (sgName) {
-  // TODO: Implement sg creation & setting SSH rule
+  const params = {
+    Description: sgName,
+    GroupName: sgName
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.createSecurityGroup(params, (err, data) => {
+        if(err){
+            reject(err)
+        }else{
+            const params = securityGroupParams(data)
+            ec2.authorizeSecurityGroupIngress(params, (err) => {
+                if(err){
+                    reject(err)
+                }else{
+                    resolve()
+                }
+            })
+        }
+    })
+
+  })
 }
 
 function createKeyPair (keyName) {
-  // TODO: Create keypair
+  const params = {
+    KeyName: keyName
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.createKeyPair(params, (err, data) => {
+        if(err) reject(err)
+        else resolve(data)
+    })
+  })
 }
 
 function createInstance (sgName, keyName) {
-  // TODO: create ec2 instance
+  console.log("create instance")
+  const params = {
+    ImageId: 'ami-0323c3dd2da7fb37d',
+    InstanceType: 't2.micro',
+    KeyName: keyName,
+    MaxCount: 1,
+    MinCount: 1,
+    SecurityGroups: [
+        sgName
+    ],
+
+
+  }
+
+  return new Promise((resolve, reject) => {
+  console.log("run instances")
+    ec2.runInstances(params, (err, data) => {
+        if(err) reject(err)
+        else resolve(data)
+    })
+  })
+
+}
+
+function securityGroupParams(data){
+    return {
+               GroupId: data.GroupId,
+               IpPermissions: [
+                   {
+                    IpProtocol: 'tcp',
+                    FromPort: 22,
+                    ToPort: 22,
+                    IpRanges: [
+                       {
+                           CidrIp: '0.0.0.0/0'
+                       }
+                    ]
+                   },
+                   {
+                    IpProtocol: 'tcp',
+                    FromPort: 3000,
+                    ToPort: 3000,
+                    IpRanges: [
+                       {
+                           CidrIp: '0.0.0.0/0'
+                       }
+                    ]
+                   }
+               ]
+           }
 }
--- scripts/module_02/ec2-startup.sh
+++ scripts/module_02/ec2-startup.sh
@@ -2,10 +2,11 @@
 curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
 sudo yum install -y nodejs
 sudo yum install -y git
-git clone https://github.com/ryanmurakami/hbfl.git
+git clone https://github.com/StutiSri/hbfl.git
 cd hbfl
+git checkout aws_test
 npm i
 npm run start
 
 # The above commands base64 encoded for entering into UserData
-# IyEvYmluL2Jhc2gNCmN1cmwgLS1zaWxlbnQgLS1sb2NhdGlvbiBodHRwczovL3JwbS5ub2Rlc291cmNlLmNvbS9zZXR1cF8xMi54IHwgc3VkbyBiYXNoIC0NCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzDQpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQNCmNkIGhiZmwNCm5wbSBpDQpucG0gcnVuIHN0YXJ0
+# IyEvYmluL2Jhc2gKY3VybCAtLXNpbGVudCAtLWxvY2F0aW9uIGh0dHBzOi8vcnBtLm5vZGVzb3VyY2UuY29tL3NldHVwXzEyLnggfCBzdWRvIGJhc2ggLQpzdWRvIHl1bSBpbnN0YWxsIC15IG5vZGVqcwpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdApnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL1N0dXRpU3JpL2hiZmwuZ2l0CmNkIGhiZmwKZ2l0IGNoZWNrb3V0IGF3c190ZXN0Cm5wbSBpCm5wbSBydW4gc3RhcnQ=
