cdb26b42834efa2acf9606a95bd8d8849a680b84
StutiSri <stutisrivastv@gmail.com>
2020-05-03 18:05:42 +0530

    Replace S3 urls with CFD urls

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://hamster-bucket-ss.s3.amazonaws.com'
+const base = 'https://d17mlbn77dqg1j.cloudfront.net'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
