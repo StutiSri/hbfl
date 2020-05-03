2bfdf3945ce679743265f1d5a1782ab3900044d4
StutiSri <stutisrivastv@gmail.com>
2020-05-04 01:33:05 +0530

    Integrate kinesis stream with lambda that saves the race results in the race table in dynamo db. Fix delete operation in sqs listener

--- scripts/module_08/create-kinesis-consumer.js
+++ scripts/module_08/create-kinesis-consumer.js
@@ -2,12 +2,12 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const lambda = new AWS.Lambda()
 const functionName = 'hamster-kinesis-stream-consumer'
-const kinesisArn = '/* TODO: Add your kinesis ARN */'
+const kinesisArn = 'arn:aws:kinesis:us-east-1:331252955957:stream/hamster-race-results-ss'
 let roleArn
 
 helpers.createLambdaKinesisRole()
@@ -44,10 +44,14 @@ function createLambda (roleArn, lambdaName, zippedCode) {
 }
 
 function createTrigger (kinesisArn, lambdaName) {
-  // TODO: Create params const for trigger
 
   return new Promise((resolve, reject) => {
-    lambda.createEventSourceMapping(params, (err, data) => {
+    lambda.createEventSourceMapping({
+      EventSourceArn: kinesisArn,
+      FunctionName: lambdaName,
+      StartingPosition: 'LATEST',
+      BatchSize: 100
+    }, (err, data) => {
       if (err) reject(err)
       else resolve(data)
     })
