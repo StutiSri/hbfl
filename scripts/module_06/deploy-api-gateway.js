71c294e299ca273ec7e976e549c245920dd4d555
StutiSri <stutisrivastv@gmail.com>
2020-05-03 17:34:44 +0530

    Deploy aPI gateway

--- scripts/module_06/deploy-api-gateway.js
+++ scripts/module_06/deploy-api-gateway.js
@@ -5,15 +5,23 @@ AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const apiG = new AWS.APIGateway()
-const apiId = '/* TODO: Add api id */'
+const apiId = 'befswy1iac'
 
 createDeployment(apiId, 'prod')
 .then(data => console.log(data))
 
 function createDeployment (apiId, stageName) {
-  // TODO: Create params const
+  const params = {
+    restApiId: apiId,
+    stageName: stageName
+  }
+
 
   return new Promise((resolve, reject) => {
-    // TODO: Create deployment
+      apiG.createDeployment(params, (err, data) => {
+        if(err) reject(err)
+        else resolve(data)
+      })
+
   })
 }
